2673774be7a0851e2969dcd3158d6085686d4e55
gabriele-annese <gannese06@gmail.com>
2021-10-21 16:17:28 +0200

    pulizia console.log

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 
 // chiedere etá
-const etá = parseInt( prompt("Inserisci la tua etá") );
-console.log(etá);
+const etá = parseInt( prompt("Inserisci la tua etá") ); 
+if((etá < 0) ||(isNaN)(etá) ) {
+    alert("informazioni non inserite corettamente")
+}
+
 // chiedere n KM
 const km =  parseInt( prompt("Inserisci i kilometri da percorrere") );
-console.log(km);
+if((km <= 0) ||(isNaN)(km) ) {
+    alert("inserire piú di 1km")
+}
+
 // prezzo al km
 const prezzo_km = 0.21;
+
 // totale tratta
 let totale_tratta = prezzo_km * km;
-console.log("totale tratta " + totale_tratta);
-// totale_tratta.toFixed(2);
 
 // sconti
 let sconto_minori =  (totale_tratta / 100 * 20);
@@ -22,15 +27,13 @@ let prezzo_scontato_over = totale_tratta - sconto_over;
 // condizioni sconti-applicati
 if (etá < 18) {
     prezzo_scontato_minori
-    console.log("prezzo scontato " + prezzo_scontato_minori );
-    document.getElementById('biglietto').innerHTML = prezzo_scontato_minori.toFixed(2);
+    document.getElementById('biglietto').innerHTML = `${prezzo_scontato_minori.toFixed(2)} €`;
 } 
 else if (etá > 65) {
     prezzo_scontato_over
-    console.log("prezzo scontato " + prezzo_scontato_over );
-    document.getElementById('biglietto').innerHTML = prezzo_scontato_over.toFixed(2);
+    document.getElementById('biglietto').innerHTML = `${prezzo_scontato_over.toFixed(2)} €`;
 }
 else {
-    document.getElementById('biglietto').innerHTML = totale_tratta.toFixed(2);
+    document.getElementById('biglietto').innerHTML = `${totale_tratta.toFixed(2)} €`;
 }
 
